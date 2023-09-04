0da70cc5332d7cdaad9b57eea3ddb9476605d9e1
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:20:34 -0300

    Add files via upload

---
+++ ProgramEx5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1POOex5
+{
+    internal class Milhas
+    {
+        private double M;
+        private double A;
+
+        public void setM(double l)
+        {
+            M = l;
+        }
+        
+        public double getM()
+        {
+            return M;
+        }
+        public double getA()
+        {
+            return A;
+        }
+
+        public void calcular()
+        {
+            A = M * 1.852;
+        }
+    }
+}
